3aa8da2ec6b59bbbc6ef11b582100aa6571bade4
Harald Pehl <harald.pehl@gmail.com>
2025-04-14 08:47:29 +0200

    Add mcup formula

--- Formula/waco.rb
+++ Formula/waco.rb
@@ -3,7 +3,7 @@ class Waco < Formula
   desc "Command line tool to manage WildFly containers"
   homepage "https://github.com/hpehl/waco"
   url "https://github.com/hpehl/waco/releases/download/v0.0.4/waco-x86_64-apple-darwin.tar.gz"
-  sha256 "6d96af279879774e048b0cafa2bb7f649a4546053bcc40eeb8030790e530492f  -"
+  sha256 "6d96af279879774e048b0cafa2bb7f649a4546053bcc40eeb8030790e530492f"
   version "0.0.4"
   license "Apache-2.0"
 
