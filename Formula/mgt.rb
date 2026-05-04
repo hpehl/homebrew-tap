ce75b407f5535e31b5be83deb254741dfdd4bd1d
Harald Pehl <harald.pehl@gmail.com>
2026-05-04 17:03:51 +0200

    feat: add on_arm/on_intel blocks for Apple Silicon support
    Update mgt and wado formulas to serve native aarch64 binaries on
    Apple Silicon and x86_64 binaries on Intel Macs.

--- Formula/mgt.rb
+++ Formula/mgt.rb
@@ -2,10 +2,18 @@
 class Mgt < Formula
   desc "Command line tool to analyze the WildFly management model"
   homepage "https://model-graph-tools.github.io/"
-  url "https://github.com/model-graph-tools/tooling/releases/download/v0.0.1/mgt-x86_64-apple-darwin.tar.gz"
-  sha256 "placeholder"
   version "0.0.1"
   license "Apache-2.0"
+
+  on_arm do
+    url "https://github.com/model-graph-tools/tooling/releases/download/v0.0.1/mgt-aarch64-apple-darwin.tar.gz"
+    sha256 "placeholder"
+  end
+
+  on_intel do
+    url "https://github.com/model-graph-tools/tooling/releases/download/v0.0.1/mgt-x86_64-apple-darwin.tar.gz"
+    sha256 "placeholder"
+  end
 
   def install
     bin.install "mgt"
--- Formula/wado.rb
+++ Formula/wado.rb
@@ -2,10 +2,18 @@
 class Wado < Formula
   desc "Command line tool to manage WildFly containers"
   homepage "https://github.com/hpehl/wado"
-  url "https://github.com/hpehl/wado/releases/download/v0.4.12/wado-x86_64-apple-darwin.tar.gz"
-  sha256 "93d3a719ff8790f3c1c0416d4d38e68d52929ecb6bbae3965f258e2bdb890edc"
   version "0.4.12"
   license "Apache-2.0"
+
+  on_arm do
+    url "https://github.com/hpehl/wado/releases/download/v0.4.12/wado-aarch64-apple-darwin.tar.gz"
+    sha256 "placeholder"
+  end
+
+  on_intel do
+    url "https://github.com/hpehl/wado/releases/download/v0.4.12/wado-x86_64-apple-darwin.tar.gz"
+    sha256 "93d3a719ff8790f3c1c0416d4d38e68d52929ecb6bbae3965f258e2bdb890edc"
+  end
 
   def install
     bin.install "wado"
